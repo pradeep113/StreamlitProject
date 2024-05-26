30b0562c525988c8053d42a4648fb67c0d7bdf93
PRADEEP KUMAR <37507656+pradeep113@users.noreply.github.com>
2024-05-26 18:16:09 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,18 @@
 import streamlit as st
 
 # Add a title to your app
-st.title("My First Streamlit App")
+st.title("Upload Invoices here")
 
+#create a file uploader
+file = st.file_uploader("pick a file")
 # Create a slider widget to select a value
-selected_value = st.slider("Select a value", 0, 100)
+#selected_value = st.slider("Select a value", 0, 100)
 
 # Calculate the square of the selected value
-squared_value = selected_value * selected_value
+#squared_value = selected_value * selected_value
 
 # Display the result
-st.write(f"{selected_value} squared is {squared_value}")
+#st.write(f"{selected_value} squared is {squared_value}")
 
 # Run the app using the following command:
 # streamlit run your_file_name.py
