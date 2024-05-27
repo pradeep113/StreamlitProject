cd5a73468b936be6c79779f3cae749dd4830dd50
PRADEEP KUMAR <37507656+pradeep113@users.noreply.github.com>
2024-05-27 14:17:44 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,16 +9,34 @@ def all_pdf_text_extract(uploaded_files):
   
   
 #*****Function to read pdf invoices and extract its data and writes on a csv file.*****
-def pdf_text_extract(uploaded_file):
+def pdf_text_extract(uploaded_file, output_csv):
   st.text(f"File name is : {uploaded_file.name}")
+  pdf_path = uploaded_file.name
+  with open(pdf_path, 'rb') as pdf_file:
+        pdf_reader = PyPDF2.PdfReader(pdf_file)
+        text = ''
+        for page in pdf_reader.pages:
+            text += page.extract_text() + '\n'
+
+    # Extract the desired lines (modify as needed)
+    selected_lines = [line for line in text.split('\n') if line.startswith("Total Due") or line.startswith("Invoice Date")] 
+    df = pd.DataFrame({'Text': [selected_lines]})
+    df.to_csv(output_csv, index=False)
+    print(selected_lines)
+    print(text)
 
 def main():
+
+  
   # Add a title to your app
   st.title("Upload Invoices here")
 
   #Define the desired path
   path = "/Users/pradeepkumar/Pictures/uploaded_files"
   file_path = Path(path)
+
+  #files to create 
+  output_csv = 'output.csv'
 
   #create a file uploader
   uploaded_files = st.file_uploader("Choose files", accept_multiple_files=True)
@@ -31,11 +49,11 @@ def main():
             #st.write(dataframe)  # Display the data (you can customize this part)
             #Create button for each file
             if st.button(f"Extract : {uploaded_file.name}"):
-               pdf_text_extract(uploaded_file)
+               pdf_text_extract(uploaded_file, output_csv)
             
               
   #Create a button labelled "Extract"
-  if st.button("extract", key="Extract"):
+  if st.button("EXTRACT", key="Extract"):
     all_pdf_text_extract(uploaded_files)
     
   # Create a slider widget to select a value
