308f207398868519eace96126a0fa7f6b213e46d
PRADEEP KUMAR <37507656+pradeep113@users.noreply.github.com>
2024-05-27 14:57:32 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,7 +45,7 @@ def main():
         for uploaded_file in uploaded_files:
             # Read the uploaded file (assuming CSV format)
             #dataframe = pd.read_csv(uploaded_file)
-            st.header("File name:", uploaded_file.name)
+            st.header(f"File name:", uploaded_file.name, "uploaded")
             #st.write(dataframe)  # Display the data (you can customize this part)
             #Create button for each file
             if st.button(f"Extract : {uploaded_file.name}"):
