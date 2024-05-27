e1bee5eb86a42d253dec08e197199e7a028b4a3e
PRADEEP KUMAR <37507656+pradeep113@users.noreply.github.com>
2024-05-27 21:31:52 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import PyPDF2
 from pathlib import Path
-import ocrmypdf
+
 
 
 
@@ -18,7 +18,6 @@ def all_pdf_text_extract(uploaded_files):
 def pdf_text_extract(uploaded_file, output_csv):
   st.text(f"File name is : {uploaded_file.name}")
   pdf_file = uploaded_file
-  #with open(pdf_path, 'rb') as pdf_file:
   pdf_reader = PyPDF2.PdfReader(pdf_file)
   text = ''
   for page in pdf_reader.pages:
@@ -55,9 +54,7 @@ def main():
   if uploaded_files:
         for uploaded_file in uploaded_files:
             # Read the uploaded file (assuming CSV format)
-            #dataframe = pd.read_csv(uploaded_file)
             st.write(f"File name:", uploaded_file.name, "uploaded")
-            #st.write(dataframe)  # Display the data (you can customize this part)
             #Create button for each file
             if st.button(f"Extract : {uploaded_file.name}"):
                pdf_text_extract(uploaded_file, output_csv)
@@ -70,15 +67,7 @@ def main():
     all_pdf_text_extract(uploaded_files)
 
     
-  # Create a slider widget to select a value
-  #selected_value = st.slider("Select a value", 0, 100)
-
-  # Calculate the square of the selected value
-  #squared_value = selected_value * selected_value
-
-  # Display the result
-  #st.write(f"{selected_value} squared is {squared_value}")
-
+ 
   # Run the app using the following command:
   #   streamlit run your_file_name.py
 
