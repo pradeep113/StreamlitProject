e26a8eb507d4ba8bbb33f0f9fc27ee993f323a49
PRADEEP KUMAR <37507656+pradeep113@users.noreply.github.com>
2024-05-27 16:37:58 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,7 +28,7 @@ def pdf_text_extract(uploaded_file, output_csv):
   df = pd.DataFrame({'Text': [selected_lines]})
   df.to_csv(output_csv, index=False)
   st.write(selected_lines)
-  st.download_button(label="Download Ouput.csv file", str(selected_lines[0]))
+  st.download_button("Download Ouput.csv file", str(selected_lines[0]))
 
 
 def main():
