f3d22a4b4836ffbaded73c16bbc52a1b766c97f3
PRADEEP KUMAR <37507656+pradeep113@users.noreply.github.com>
2024-05-29 12:56:32 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,7 +41,7 @@ def pdf_text_extract(uploaded_file, output_csv):
   #df.to_csv(output_csv, index=False)
   st.write(selected_lines)
   #if selected_line is not none:
-  st.download_button("Download Ouput file", str(selected_lines_re, selected_lines))
+  st.download_button("Download Ouput file", str(selected_lines))
 
   #ocrmypdf.ocr(pdf_file, 'put.pdf', skip_text=True)
   #print('File converted successfully!')
